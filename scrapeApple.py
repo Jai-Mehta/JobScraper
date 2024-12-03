d49c635553c0e1c55bb2b8ed519008cc497df80e
Jai-Mehta <49522548+Jai-Mehta@users.noreply.github.com>
2024-12-02 21:00:21 -0500

    Update scrapeApple.py

--- scrapeApple.py
+++ scrapeApple.py
@@ -30,7 +30,7 @@ def get_all_jobs(pages):
         if response.status_code != 200:
             warn(f"Request: {requests}; Status code: {response.status_code}")
 
-        if requests > 108:
+        if requests > 10:
             warn("Number of requests exceeded the limit.")
             break
 
